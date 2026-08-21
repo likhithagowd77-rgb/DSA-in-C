fb6ac289412139c56764915a718ee622a88f9c7d
likhithagowd77-rgb <likhithagowd77@gmail.com>
2026-08-21 15:50:25 +0000

    maximum value usin array

--- 02_max_array.c
+++ 02_max_array.c
@@ -1,16 +1,21 @@
-#include<stdio.h>
+#include <stdio.h>
+
 int main()
 {
-  int arr[5]={10,20,30,40,50};
-int max,i;
-max=arr[0];
-for(i=1;i<n;i++)
-{
-if(arr[i]>max)
-{
-max=arr[i];
-printf("%d",max);
-}
-}
-return 0;
+    int arr[5] = {10, 20, 30, 40, 50};
+    int max, i;
+
+    max = arr[0];
+
+    for(i = 1; i < 5; i++)
+    {
+        if(arr[i] > max)
+        {
+            max = arr[i];
+        }
+    }
+
+    printf("Maximum = %d", max);
+
+    return 0;
 }
